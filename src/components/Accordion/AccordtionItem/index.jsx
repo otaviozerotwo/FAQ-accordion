51e9e704b788d1bc796fbe18dc0f6151e64a9173
Otavio <otavio.zerotwo@gmail.com>
2026-09-20 21:36:43 -0300

    fix: add atributes aria-controls & aria-expanded AccordionItem component

--- src/components/Accordion/AccordtionItem/index.jsx
+++ src/components/Accordion/AccordtionItem/index.jsx
@@ -1,19 +1,30 @@
 import iconPlus from '/assets/images/icon-plus.svg';
 import iconMinus from '/assets/images/icon-minus.svg';
 
-const AccordionItem = ({ title, content, isOpen, onToggle }) => {
+const AccordionItem = ({ id, title, content, isOpen, onToggle }) => {
   return (
     <div className='border-b-2 border-b-purple-100 last:border-b-0'>
-      <div className='w-full py-6 flex justify-between items-center gap-4'>
-        <span className='text-base/tight font-semibold text-start shrink'>{title}</span>
-        <button onClick={onToggle} className='h-8 w-8 shrink-0 cursor-pointer'>
-          <img className='h-8 w-8' src={!isOpen ? iconPlus : iconMinus} alt="" />
-        </button>
-      </div>
+      <button
+        onClick={onToggle}
+        className='w-full py-6 flex justify-between items-center gap-4 cursor-pointer'
+        aria-controls={`faq-content-${id}`}
+        aria-expanded={isOpen ? 'true' : 'false'}
+      >
+        <span
+          className='text-base/tight font-semibold text-start shrink hover:text-purple-700'
+        >
+          {title}
+        </span>
+
+        <img
+          className='h-8 w-8' src={!isOpen ? iconPlus : iconMinus} alt=""
+          aria-hidden='true'
+        />
+      </button>
       <div
         className={`grid transition-all duration-300 ease-in-out ${isOpen ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}
       >
-        <div className='overflow-hidden'>
+        <div id={`faq-content-${id}`} className='overflow-hidden'>
           <p className='pb-4 text-purple-600'>{content}</p>
         </div>
       </div>
--- src/components/Accordion/index.jsx
+++ src/components/Accordion/index.jsx
@@ -6,18 +6,22 @@ const Accordion = () => {
 
   const items = [
     {
+      id: 1,
       title: 'What is Frontend Mentor, and how will it help me?',
       content: 'Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It\'s suitable for all levels and ideal for portfolio building.'
     },
     {
+      id: 2,
       title: 'Is Frontend Mentor free?',
       content: 'Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.'
     },
     {
+      id: 3,
       title: 'Can I use Frontend Mentor projects in my portfolio?',
       content: 'Yes, you can use projects completed on Frontend Mentor in your portfolio. It\'s an excellent way to showcase your skills to potential employers!'
     },
     {
+      id: 4,
       title: 'How can I get help if I\'m stuck on a challenge?',
       content: 'The best place to get help is inside Frontend Mentor\'s Discord community. There\'s a help channel where you can ask questions and seek support from other community members.'
     },
@@ -29,15 +33,19 @@ const Accordion = () => {
 
   return (
     <div>
-      {items.map((item, index) => (
-        <AccordionItem
-          key={index}
-          title={item.title}
-          content={item.content}
-          isOpen={openIndex === index}
-          onToggle={() => handleToggle(index)}
-        />
-      ))}
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            <AccordionItem
+              id={item.id}
+              title={item.title}
+              content={item.content}
+              isOpen={openIndex === index}
+              onToggle={() => handleToggle(index)}
+            />
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
